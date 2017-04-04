2e0a7439c3cde083ee88b71e328ffc824db1ee10
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-04 14:56:13 -0500

    added exercise 3

--- higher-order/higher-order.js
+++ higher-order/higher-order.js
@@ -1,49 +1,62 @@
-function repeat(fn, n) {
-let i = 0;
-  while (i < n) {
-    fn();
-    i++;
-  }
+// function repeat(fn, n) {
+// let i = 0;
+//   while (i < n) {
+//     fn();
+//     i++;
+//   }
+// }
+// function hello() {
+//   console.log('hello world');
+// }
+// function goodbye() {
+//   console.log('goodbye world');
+// }
+//
+// repeat(hello, 5);
+// repeat(goodbye, 5);
+//
+// function filter(arr, fn){
+//   let newArray = [];
+//   for (let i=0; i < arr.length; i++){
+//   	let currentElement = arr[i];
+//   	if (fn(currentElement)  ){
+//   		newArray.push(currentElement);
+//   	}
+//   }
+//   return newArray;
+// }
+//
+// function anaFun(arg){
+// 	if(arg.length === 3) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// };
+//
+//
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// const newFilteredArray = filter(myNames, anaFun);
+// console.log(newFilteredArray);
+
+
+
+
+function hazardWarningCreator(typeOfWarning) {
+  let warningCounter = 0;
+
+  return function(location) {
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  };
 }
-function hello() {
-  console.log('hello world');
-}
-function goodbye() {
-  console.log('goodbye world');
-}
 
-repeat(hello, 5);
-repeat(goodbye, 5);
+const rocksWarning = hazardWarningCreator('Rocks on the Road at');
+const iceWarning = hazardWarningCreator('Ice on the Road');
+const bodiesWarning = hazardWarningCreator('Dead bodies on the Road');
 
-function filter(arr, fn){
-  let newArray = [];
-  for (let i=0; i < arr.length; i++){
-  	let currentElement = arr[i];
-  	if (fn(currentElement)  ){
-  		newArray.push(currentElement);
-  	}
-  }
-  return newArray;	
-}
-
-function anaFun(arg){
-	if(arg.length === 3) {
-		return true;
-	} else {
-		return false;
-	}
-};
-
-
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
-const newFilteredArray = filter(myNames, anaFun);
-console.log(newFilteredArray);
-
-
-/*const filteredNames = filter(myNames, function(name){
-    // This is a "predicate function" - it's a function that only returns a boolean
-    return name[0] === 'R';
-});
-
-console.log(filteredNames) // => ['Rich', 'Ray']
-*/
+rocksWarning('71st Street');
+rocksWarning('81st Street');
+iceWarning('Elm Street');
+bodiesWarning('Main Street');
