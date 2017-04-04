8c65e6e3a944f710eab383a67946066894497ad0
htwright <harrisontwright@gmail.com>
2017-04-04 10:42:59 -0500

    this kills chrome

---
+++ loop-twists/max-and-min.js
@@ -0,0 +1,19 @@
+// without sort or math.max/min and with a while loop
+
+function max(numbers){
+	let i = 0;
+	while (i < numbers.length){
+		let currentMax = 0;
+		if (numbers[i] > max){
+			currentMax = numbers[i];
+		}
+	}
+	return currentMax;
+}
+
+function min(numbers){
+
+}
+let numList = [1, 2, 3, 4, 5 , 6];
+const list = max(numList);
+console.log(list);
