37296a6b18d843e0ef7a9b79090980cea61a8b3a
htwright <harrisontwright@gmail.com>
2017-04-04 14:28:45 -0500

    really fixed 2nd arg

--- higher-order/higher-order.js
+++ higher-order/higher-order.js
@@ -14,3 +14,36 @@ function goodbye() {
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+function filter(arr, fn){
+  let newArray = [];
+  for (let i=0; i < arr.length; i++){
+  	let currentElement = arr[i];
+  	if (fn(currentElement)  ){
+  		newArray.push(currentElement);
+  	}
+  }
+  return newArray;	
+}
+
+function anaFun(arg){
+	if(arg.length === 3) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+const newFilteredArray = filter(myNames, anaFun);
+console.log(newFilteredArray);
+
+
+/*const filteredNames = filter(myNames, function(name){
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+*/
