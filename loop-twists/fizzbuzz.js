a074433f889dda72f163739fb17cd81015097edc
htwright <harrisontwright@gmail.com>
2017-04-04 14:23:24 -0500

    func as arg 2

--- loop-twists/fizzbuzz.js
+++ loop-twists/fizzbuzz.js
@@ -1,22 +1,31 @@
 function fizzBuzz(num){
 
     for (i = 1; i <= num; i++){
-	  switch (true) {
-	  	case i%3==0 && i%5==0:
-		  console.log('fizzBuzz');
-		  break;
-		case i%3==0:
-		  console.log('fizz');
-		  break;
-		case i%5==0:
-		  console.log('buzz');
-		  break;
-		default:
-		  console.log(i);
-		  break;
+    	if (i % 5 == 0 && i % 3 == 0){
+    		let j = 3;
+    	} else if (i % 5 == 0){
+    		let j = 2;
+    	} else if (i % 3 == 0){
+    		let j = 1;
+    	} else {
+    		let j = 0;
+    	}
+			  switch () {
+			  	case j=3:
+				  console.log('fizzBuzz');
+				  break;
+				case j=2:
+				  console.log('fizz');
+				  break;
+				case j=1:
+				  console.log('buzz');
+				  break;
+				default:
+				  console.log(i);
+				  break;
+			
+
 		}
-
-
 	}
 }
 fizzBuzz(15);
