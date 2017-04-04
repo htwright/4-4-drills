5c57584e8020aca41d37df90ad379eed159f2a8e
htwright <harrisontwright@gmail.com>
2017-04-04 10:49:02 -0500

    max and min done

--- loop-twists/max-and-min.js
+++ loop-twists/max-and-min.js
@@ -2,18 +2,31 @@
 
 function max(numbers){
 	let i = 0;
+	let currentMax = 0;
+
 	while (i < numbers.length){
-		let currentMax = 0;
-		if (numbers[i] > max){
+		if (numbers[i] > currentMax){
 			currentMax = numbers[i];
 		}
+		i++;
 	}
 	return currentMax;
 }
 
 function min(numbers){
+	let i = 0;
+	let currentMin = numbers[0];
 
+	while (i < numbers.length){
+		if (numbers[i] < currentMin){
+			currentMin = numbers[i];
+		}
+		i++;
+	}
+	return currentMin;
 }
-let numList = [1, 2, 3, 4, 5 , 6];
+let numList = [1, 2, 9, 4, 5 , 6];
 const list = max(numList);
 console.log(list);
+const list1= min(numList);
+console.log(list1);
